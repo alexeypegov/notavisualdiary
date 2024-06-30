de85edba8e69fd6a48d99e1dc782ddc5914ff219
Alexey Pegov <alexey@pegov.dev>
2024-06-30 22:47:57 +0200

    new posts

--- new.sh
+++ new.sh
@@ -4,14 +4,17 @@ files=$(ls posts/*.md | sort)
 last_file=$(echo "$files" | tail -n 1)
 last_filename=$(basename "$last_file" .md)
 next_number=$((10#${last_filename} + 1))
-filename=$(printf "posts/%03d.md" $next_number)
+prefixed_number=$(printf "%03d" $next_number)
+filename=$(printf "posts/%s.md" $prefixed_number)
 
-cat <<EOF > "${filename}"
+cat <<EOL > "${filename}"
 ---
-Title: #$next_number
+Title:
 Date: $(date +'%Y-%m-%d')
 Orientation: vertical
-Cover: 
+Cover: images/$prefixed_number-.webp
 ---
 
-EOF 
+![](images/$prefixed_number-@2x.webp)
+
+EOL
